89733901dabf6f7f24400cf7d9e326bfed8901c9
ejendret <ejendret@calpoly.edu>
2023-09-25 12:28:57 -0700

    Initial commit for form

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,24 +1,15 @@
 // src/MyApp.js
 import React, {useState} from 'react';
 import Table from './Table';
+import Form from './Form';
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Bob',
-        job: 'Builder',
-      }
-      // the rest of the data
-    ]); 
+  const [characters, setCharacters] = useState([]); 
 
-    function removeOneCharacter (index) {
-	    const updated = characters.filter((character, i) => {
-	        return i !== index
-	    });
+  function removeOneCharacter (index) {
+    const updated = characters.filter((character, i) => {
+        return i !== index
+    });
 	  setCharacters(updated);
 	}
     
@@ -26,6 +17,7 @@ function MyApp() {
 	<div className="container">
 	    <Table characterData={characters}
         removeCharacter = {removeOneCharacter} />
+      <Form />
      </div>
   ) 
 }
