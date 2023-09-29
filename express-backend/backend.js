4c0d3a821d898cd11323836d2c5d13c3da3ef761
ejendret <ejendret@calpoly.edu>
2023-09-29 11:46:10 -0700

    Implemented 201 http code

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -19,20 +19,20 @@ app.get('/', (req, res) => {
 app.get('/users', (req, res) => {
     const name = req.query.name;
     const job = req.query.job;
-    if (name != undefined && job != undefined){
+    if (name != undefined && job != undefined) {
         let result = findUserByNameAndJob(name, job);
-        result = {users_list: result};
-	res.send(result);
+        result = { users_list: result };
+        res.send(result);
     }
-    else if (job != undefined){
+    else if (job != undefined) {
         let result = findUserByJob(job);
-	result = {users_list: result};
-	res.send(result);
+        result = { users_list: result };
+        res.send(result);
     }
-    else if (name != undefined){
+    else if (name != undefined) {
         let result = findUserByName(name);
-	result = {users_list: result};
-	res.send(result);
+        result = { users_list: result };
+        res.send(result);
     }
     else {
         res.send(users);
@@ -43,22 +43,22 @@ app.get('/users', (req, res) => {
 app.post('/users', (req, res) => {
     const userToAdd = req.body;
     addUser(userToAdd);
-    res.status(200).end();
+    res.status(201).end();
 });
 
 // Helper
-const findUserByName = (name) => { 
-    return users['users_list'].filter( (user) => user['name'] === name); 
+const findUserByName = (name) => {
+    return users['users_list'].filter((user) => user['name'] === name);
 }
 
-const findUserByJob = (job) => { 
-    return users['users_list'].filter( (user) => user['job'] === job); 
+const findUserByJob = (job) => {
+    return users['users_list'].filter((user) => user['job'] === job);
 }
 
-const findUserByNameAndJob = (name, job) => { 
-    return users['users_list'].filter( (user) => user['name'] === name).filter( (user) => user['job'] === job);
+const findUserByNameAndJob = (name, job) => {
+    return users['users_list'].filter((user) => user['name'] === name).filter((user) => user['job'] === job);
 }
-function addUser(user){
+function addUser(user) {
     users['users_list'].push(user);
 }
 
@@ -69,7 +69,7 @@ app.get('/users/:id', (req, res) => {
     if (result === undefined || result.length == 0)
         res.status(404).send('Resource not found.');
     else {
-        result = {users_list: result};
+        result = { users_list: result };
         res.send(result);
     }
 });
@@ -78,20 +78,20 @@ app.delete('/users/:id', (req, res) => {
     const id = req.params['id'];
     let result = findIndexById(id);
     if (result === undefined || result.length == 0)
-	res.status(404).send('Resource not found.');
+        res.status(404).send('Resource not found.');
     else {
-    	users['users_list'].splice(result, 1);
+        users['users_list'].splice(result, 1);
     }
 });
 
 // Helper
 function findUserById(id) {
-    return users['users_list'].find( (user) => user['id'] === id); // or line below
+    return users['users_list'].find((user) => user['id'] === id); // or line below
     //return users['users_list'].filter( (user) => user['id'] === id);
 }
 
-function findIndexById(id){
-    return users['users_list'].findIndex( (user) => user['id'] === id); // or line below
+function findIndexById(id) {
+    return users['users_list'].findIndex((user) => user['id'] === id); // or line below
     //return users['users_list'].filter( (user) => user['id'] === id);
 }
 
@@ -101,32 +101,32 @@ app.listen(port, () => {
 
 // Users
 const users = {
-   users_list :
-   [
-      {
-         id : 'xyz789',
-         name : 'Charlie',
-         job: 'Janitor',
-      },
-      {
-         id : 'abc123',
-         name: 'Mac',
-         job: 'Bouncer',
-      },
-      {
-         id : 'ppp222',
-         name: 'Mac',
-         job: 'Professor',
-      },
-      {
-         id: 'yat999',
-         name: 'Dee',
-         job: 'Aspring actress',
-      },
-      {
-         id: 'zap555',
-         name: 'Dennis',
-         job: 'Bartender',
-      }
-   ]
+    users_list:
+        [
+            {
+                id: 'xyz789',
+                name: 'Charlie',
+                job: 'Janitor',
+            },
+            {
+                id: 'abc123',
+                name: 'Mac',
+                job: 'Bouncer',
+            },
+            {
+                id: 'ppp222',
+                name: 'Mac',
+                job: 'Professor',
+            },
+            {
+                id: 'yat999',
+                name: 'Dee',
+                job: 'Aspring actress',
+            },
+            {
+                id: 'zap555',
+                name: 'Dennis',
+                job: 'Bartender',
+            }
+        ]
 }
--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,48 +1,64 @@
 // src/MyApp.js
 import axios from 'axios';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from './Table';
 import Form from './Form';
 
 function MyApp() {
-  const [characters, setCharacters] = useState([]); 
+  const [characters, setCharacters] = useState([]);
 
-  function removeOneCharacter (index) {
+  function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
-        return i !== index
+      return i !== index
     });
-	  setCharacters(updated);
-	}
-
-  function updateList(person) {
-    setCharacters([...characters, person]);
+    setCharacters(updated);
   }
 
-  async function fetchAll(){
+  // Backend link functions
+  async function fetchAll() {
     try {
       const response = await axios.get('http://localhost:8000/users');
-      return response.data.users_list;     
+      return response.data.users_list;
     }
-     catch (error){
-       //We're not handling errors. Just logging into the console.
-       console.log(error); 
-       return false;         
+    catch (error) {
+      //We're not handling errors. Just logging into the console.
+      console.log(error);
+      return false;
     }
   }
+
   useEffect(() => {
-    fetchAll().then( result => {
+    fetchAll().then(result => {
       if (result)
-         setCharacters(result);
-      });
-  }, [] );
-    
+        setCharacters(result);
+    });
+  }, []);
+
+  async function makePostCall(person) {
+    try {
+      const response = await axios.post('http://localhost:8000/users', person);
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  function updateList(person) {
+    makePostCall(person).then(result => {
+      if (result && result.status === 201)
+        setCharacters([...characters, person]);
+    });
+  }
+
   return (
-	<div className="container">
-	    <Table characterData={characters}
-        removeCharacter = {removeOneCharacter} />
+    <div className="container">
+      <Table characterData={characters}
+        removeCharacter={removeOneCharacter} />
       <Form handleSubmit={updateList} />
-     </div>
-  ) 
+    </div>
+  )
 }
 
 export default MyApp;
